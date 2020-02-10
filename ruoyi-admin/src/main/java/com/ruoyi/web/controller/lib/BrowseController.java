6809a0450389cc478f9f660f73ca8cfad877bc70
虞秋元 <yuqiuyuan@gokuaidian.com>
2020-02-10 10:54:39 +0800

    文档预览页面+测试环境配置文件

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/lib/BrowseController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/lib/BrowseController.java
@@ -1,19 +1,15 @@
 package com.ruoyi.web.controller.lib;
 
-import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.constant.UserConstants;
 import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
-import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.framework.util.ShiroUtils;
+import com.ruoyi.system.domain.LibDoc;
 import com.ruoyi.system.domain.LibTag;
+import com.ruoyi.system.service.ILibDocService;
 import com.ruoyi.system.service.ILibTagService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,15 +27,23 @@ public class BrowseController extends BaseController {
 
     private static String PREFIX = "/lib/browse";
     private final ILibTagService libTagService;
+    private final ILibDocService libDocService;
 
     @Autowired
-    public BrowseController(ILibTagService libTagService) {
+    public BrowseController(ILibTagService libTagService, ILibDocService libDocService) {
         this.libTagService = libTagService;
+        this.libDocService = libDocService;
     }
 
     @RequiresPermissions("lib:browse:view")
     @GetMapping()
-    public String browse() {
+    public String browse(ModelMap mmap) {
+        final LibTag query = new LibTag();
+        query.setTagStatus("0");
+        final List<LibTag> libTags = libTagService.selectTagList(query);
+        mmap.put("tagList", libTags);
+        final List<LibDoc> libDocs = libDocService.selectDocList(new LibDoc());
+        mmap.put("docList", libDocs);
         return PREFIX + "/browseH5";
     }
 
